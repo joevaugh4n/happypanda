a06b6afb1714e57c515e1878c91c655cd0e2edfc
Joe Vaughan <jbvaughan1993@gmail.com>
2025-01-24 17:43:32 +0000

    updated footer links

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,12 +1,20 @@
-import { FaWhatsapp, FaInstagram, FaWeixin } from "react-icons/fa";
+import { FaPhone, FaInstagram, FaWeixin } from "react-icons/fa";
 import SocialIcon from "../atoms/social-icon";
 
 export default function Nav() {
   return (
     <ul className="flex justify-end gap-2">
-      <SocialIcon icon={<FaWhatsapp />} title="WhatsApp" url="test" />
-      <SocialIcon icon={<FaInstagram />} title="Instagram" url="test" />
-      <SocialIcon icon={<FaWeixin />} title="Weibo" url="test" />
+      <SocialIcon
+        icon={<FaInstagram />}
+        title="Instagram"
+        url="https://instagram.com/kungfureading"
+      />
+      <SocialIcon
+        icon={<FaWeixin />}
+        title="Weibo"
+        url="weixin://dl/chat?{Yipin-Joanna521}"
+      />
+      <SocialIcon icon={<FaPhone />} title="Call" url="tel:07587 577966" />
     </ul>
   );
 }
