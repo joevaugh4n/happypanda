2927a9ad491dde4cb390838065c6e1832eaed486
Joe Vaughan <jbvaughan1993@gmail.com>
2025-01-24 18:02:31 +0000

    small fix

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,10 +4,10 @@ import logo from "/assets/logo.webp";
 export default function Header() {
   return (
     <>
-      <div className="flex md:flex-row flex-col-reverse justify-between md:items-start margin">
+      <div className="flex md:flex-row flex-col-reverse justify-between md:items-start">
         <a
           href="/"
-          className="md:w-2/5 md:h-auto inline-block w-[80%] h-auto self-center hover:border"
+          className="md:w-2/5 md:h-auto inline-block h-auto self-center hover:border lg:ml-16"
           title="Happy Panda"
         >
           <img className="lg:mx-0 mx-auto" src={logo} alt="Logo" />
